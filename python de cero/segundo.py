8a3ad2d3f41acfe4ff17a94f8a4005277afc0bc0
dantez <dzurlo@alumno.huergo.edu.ar>
2026-03-05 15:41:29 -0300

    los primeros de muchos

---
+++ python de cero/segundo.py
@@ -0,0 +1,19 @@
+import random
+
+personas = ["maduro", "trump", "binladen"]
+
+def main():
+    persinvs()
+    perchau()
+
+def persinvs():
+    for i in personas:
+        print(f"hola {i} veni a la joda")
+
+def perchau():
+    perno = random.choice (personas)
+    print(f"{perno} no puede venir")
+
+
+
+main()
