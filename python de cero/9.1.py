3a47cd796bfd7dc242c0cf82639de240f6683bcc
whyalwaysobama <dzurlo@alumno.huergo.edu.ar>
2026-03-11 09:54:14 -0300

    9.1

---
+++ python de cero/9.1.py
@@ -0,0 +1,21 @@
+ubicaciones = ["LA", "BSAS", "Avellaneda", "Gerli", "Lanus"]
+
+print(ubicaciones)
+ 
+print(sorted(ubicaciones))      
+
+print(sorted(ubicaciones, reverse=True))
+
+print(ubicaciones)
+
+ubicaciones.reverse()
+print(ubicaciones)
+
+ubicaciones.reverse()
+print(ubicaciones)
+
+ubicaciones.sort()
+print(ubicaciones)
+
+ubicaciones.sort()
+print(ubicaciones)
